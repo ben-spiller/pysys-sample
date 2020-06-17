e75eadf38ea6a0a64d63800796092630bfc5455b
ben-spiller <ben-spiller@users.noreply.github.com>
2020-06-17 12:37:39 +0100

    add more annotation tests

--- test/correctness/MyApp_cor_002/run.py
+++ test/correctness/MyApp_cor_002/run.py
@@ -5,6 +5,10 @@ from pysys.utils.logutils import ColorLogFormatter, stdoutPrint
 class PySysTest(BaseTest):
 	def execute(self):
 		stdoutPrint('::error file=test/correctness/MyApp_cor_002/run.py,line=9,col=6::This is a test failure in 002')
+		stdoutPrint('::error::This is a 1 multi-line%0Amessage')
+		stdoutPrint('::error::This is a 2 multi-line\\nmessage')
+		stdoutPrint('::error::This is a 3 message with :: colons : all over the place\\oh yes!')
+
 		stdoutPrint(r'::error file=test\correctness\MyApp_cor_002\run.py,line=5,col=6::This is another test failure')
 		stdoutPrint('::error file=test/correctness/MyApp_cor_001/run.py,line=2,col=6::This is a test failure 001')
 		
