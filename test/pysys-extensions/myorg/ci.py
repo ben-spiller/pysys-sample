4f9dd8998d26ec61f4a0150523aeca955cf10a66
ben-spiller <ben-spiller@users.noreply.github.com>
2020-06-23 21:40:06 +0100

    CI improvements

--- test/pysys-extensions/myorg/ci.py
+++ test/pysys-extensions/myorg/ci.py
@@ -160,14 +160,16 @@ class GitHubActionsCIWriter(BaseResultsSummaryCIWriter):
 		super(GitHubActionsCIWriter, self).processResult(testObj, cycle=cycle, testTime=testTime, 
 			testStart=testStart, runLogOutput=runLogOutput, **kwargs)
 		
-		if self.remainingAnnotations > 0:
-			# TODO: only for errors
+		if self.remainingAnnotations > 0 and testObj.getOutcome() in FAILS:
 			# TODO: try to include line number if possible
 			# Currently, GitHub actions doesn't show the annotation against the source code unless the specified line 
 			# number is one of the lines changes or surrounding context lines, but we do the best we can
 			msg = stripColorEscapeSequences(runLogOutput)
 			self.remainingAnnotations -= 1
 			if self.remainingAnnotations == 0: msg += '\n(annotation limit reached; for any additional test failures, see the detailed log)'
-			self.outputGitHubCommand(u'error', msg, params='file='+os.path.join(testObj.descriptor.testDir, testObj.descriptor.module).replace('\\','/'))
+			self.outputGitHubCommand(u'group', u'(GitHub annotation)')
+			self.outputGitHubCommand(u'warning', msg, params=u'file='+os.path.join(testObj.descriptor.testDir, testObj.descriptor.module).replace('\\','/'))
+			self.outputGitHubCommand(u'endgroup')
+
 			
 
